a0047a6910158994322728673b1bd1bfc3e005cd
Kala S Pai <kalaspai@gmail.com>
2019-02-03 18:40:52 +0000

    Code Refactor

--- ImageInfoStandard/api/ImageInfoController.cs
+++ ImageInfoStandard/api/ImageInfoController.cs
@@ -15,17 +15,6 @@ namespace ImageInfoStandard
         {
 
         }
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
 
         public ImageInfo Get(string path)
         {
@@ -56,22 +45,6 @@ namespace ImageInfoStandard
             result.Type = image.GetType().Name;
 
             return result;
-        }
-
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
         }
     }
 
